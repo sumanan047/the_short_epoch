5dc13215ac24c74876d82f36510567e45edf8a38
Suman Saurabh <sumanan047@gmail.com>
2025-01-11 16:39:41 -0600

    Refactor Flask app and add logo functionality

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def home():
--- flask_app/logo.py
+++ flask_app/logo.py
@@ -4,21 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.lines import Line2D
+from PIL import Image
 
-def create_logo(output_dir='.'):
+def create_logo(lw = 10, output_dir='.'):
     # Create a figure and axis
     fig, ax = plt.subplots(figsize=(5, 5)) # Increased figure size for higher resolution
     # Add a white circle patch
-    circle = patches.Circle((0.5, 0.5), 0.25, linewidth=10, edgecolor='r', facecolor='none')
+    circle = patches.Circle((0.5, 0.5), 0.25, linewidth=2*lw, edgecolor='r', facecolor='none')
     ax.add_patch(circle)
     # Add a black arc patch
-    arc = patches.Arc((0.5, 0.5), 0.5, 0.5, theta1=90, theta2=270, linewidth=30, edgecolor='black', facecolor='none')
+    arc = patches.Arc((0.5, 0.5), 0.5, 0.5, theta1=90, theta2=270, linewidth=3*lw, edgecolor='black', facecolor='none')
     ax.add_patch(arc)
     # Add a red line from the center with orientation 270 degrees
-    line = Line2D([0.5, 0.5], [0, 0.5], linewidth=10, color='red')
+    line = Line2D([0.5, 0.5], [0, 0.5], linewidth=2*lw, color='red')
     ax.add_line(line)  # Use add_line instead of add_patch for Line2D
     # Add a black line from the center with orientation 90 degrees
-    line = Line2D([0.5, 0.5], [0.5, 1.0], linewidth=30, color='black')
+    line = Line2D([0.5, 0.5], [0.5, 1.0], linewidth=3*lw, color='black')
     ax.add_line(line)  # Use add_line instead of add_patch for Line2D
     # Set axis limits
     ax.set_xlim([0, 1])
@@ -41,7 +42,29 @@ def remove_background(input_path, output_path):
     img_transparent = Image.fromarray(img_np)
     img_transparent.save(output_path)
 
+def png_to_ico(png_path, ico_path):
+    """
+    Converts a PNG image to an ICO file.
+
+    Args:
+        png_path: Path to the input PNG image file.
+        ico_path: Path to the output ICO file.
+        sizes: A list of icon sizes to include in the ICO file.
+    """
+    # Load the PNG image
+    png_path = 'static/images/theshortepochlogo.png'
+    img = Image.open(png_path)
+
+    # Resize the image to 16x16
+    img_resized = img.resize((32, 32))
+
+    # Save the resized image as ICO
+    ico_path = 'static/images/theshortepochlogo.ico'
+    img_resized.save(ico_path, format='ICO')
+
+
 
 if __name__ == "__main__":
-    create_logo(output_dir='static/images')
+    create_logo(lw=20,output_dir='static/images')
     remove_background('static/images/theshortepochlogo.png', 'static/images/theshortepochlogo.png')
+    png_to_ico('static/images/theshortepochlogo.png', 'static/images/theshortepochlogo.ico')
